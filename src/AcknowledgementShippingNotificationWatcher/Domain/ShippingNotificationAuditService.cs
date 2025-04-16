5b9cfddf52c44e3594e49423d4d06f7eec1c0592
Vilis Trautmanis <vilis.trautmanis@gmail.com>
2025-04-16 08:19:09 +0300

    Adding ShippingNotificationAuditService with AuditNotification method.

---
+++ src/AcknowledgementShippingNotificationWatcher/Domain/ShippingNotificationAuditService.cs
@@ -0,0 +1,25 @@
+using AcknowledgementShippingNotificationWatcher.Domain.NotificationInputs;
+using AcknowledgementShippingNotificationWatcher.Domain.ShippingNotificationConverters;
+using AcknowledgementShippingNotificationWatcher.Domain.ShippingNotificationConverters.Results;
+
+namespace AcknowledgementShippingNotificationWatcher.Domain;
+
+public class ShippingNotificationAuditService(IShippingNotificationAuditRepository repository)
+{
+    public void AuditNotification(AcknowledgementShippingNotificationInput notificationDto)
+    {
+        var result = AcknowledgementShippingNotificationConverter.Convert(notificationDto);
+        switch (result)
+        {
+            case AcknowledgementShippingNotificationFailedConvertResult failedResult:
+                // It is likely that here we would not want to throw but instead log an error or make it known to a user
+                // that we encountered a corrupt record.
+                throw new FormatException($"Failed to convert shipping notification input. Reason: {failedResult.Reason}");
+            case AcknowledgementShippingNotificationSuccessConvertResult successResult:
+                repository.Save(successResult.AcknowledgementShippingNotification);
+                break;
+            default:
+                throw new Exception("Unexpected result from AcknowledgementShippingNotificationConverter");
+        }
+    }
+}
